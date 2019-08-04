31183f9e54d4b16d42f1b7324aba844dd0681d0a
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-08-04 09:28:31 +0545

    refactor app to class based component..

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,30 @@
-import React from "react";
-import {BrowserRouter , Route} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing..</h2>;
 
-const App =() =>{
-  return (
-    <div>
-     <BrowserRouter>
-      <div className="container">
-        <Header/>
-        <Route exact  path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route path="/surveys/new" component={SurveyNew} />
+class App extends Component {
+
+  componentDidMount(){
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
       </div>
-     </BrowserRouter>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
