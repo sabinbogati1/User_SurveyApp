a6ad0af220e7f3ee089c9b23a5551e5fc8569028
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-08-06 08:39:52 +0545

    Finalizing Charges..

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,15 +3,18 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app =>{
 
-  app.post("/api/stripe", (req,res) => {
+  app.post("/api/stripe", async (req,res) => {
       console.log("req.body :: ", req.body);
 
-      stripe.charges.create({
+    const charge = await stripe.charges.create({
         amount: 500,
         currency: "usd",
         description: "$5 for 5 credits",
         source: req.body.id
-      })
+      });
+
+
+      console.log("charge :: ", charge);
 
   });
 
