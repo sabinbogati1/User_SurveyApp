6f3c777f1466513c6f60aaf7ce14d2bd4b014f67
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-08-06 09:15:31 +0545

    Added Route Specific Middleware..

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,16 +1,15 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app =>{
 
-  app.post("/api/stripe", async (req,res) => {
-      console.log("req.body :: ", req.body);
-
-      if(!req.user){
-        return res.status(401).send({
-          error: "You must log in!"
-        });
-      }
+  app.post("/api/stripe", requireLogin, async (req,res) => {
+      // if(!req.user){
+      //   return res.status(401).send({
+      //     error: "You must log in!"
+      //   });
+      // }
 
 
     const charge = await stripe.charges.create({
